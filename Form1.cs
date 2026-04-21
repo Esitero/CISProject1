df394697a904ab0d67d1bb40193c77a939d8c772
Esitero <esitero671@my.pfeiffer.edu>
2026-04-21 11:56:04 -0400

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,4 @@
-namespace Project_1
+namespace Project_CIS
 {
     public partial class Form1 : Form
     {
@@ -6,10 +6,52 @@ namespace Project_1
         {
             InitializeComponent();
         }
+        double total = 0;
+        private void checkBox9_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double total = 0;
+
+
+            if (checkBox5.Checked)
+                total += 10.00;
+            else if (checkBox6.Checked)
+                total += 30.00;
+            else if (checkBox7.Checked)
+                total += 19.00;
+            else if (checkBox6.Checked)
+                total += 30.00;
+            else if (checkBox7.Checked)
+                total += 19.00;
+
+            label2.Text = l
+            label2.Text = "Total: $" + total.ToString("0.00");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("We created a Window");
+            Form myForm = new Form();
+            myForm.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("You owe us " + total.ToString("0.00"));
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
